d6c35659b80419c3b9a88f0ddabb2a1139150cf0
RiadAliyev38 <aliyevriad38@gmail.com>
2025-12-04 18:59:36 +0400

    add models

---
+++ Magazine/Magazine/Courier.cs
@@ -0,0 +1,16 @@
+﻿namespace Magazine;
+
+public class Courier
+{
+    private static int _count;
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool IsAvailable { get; set; }
+    public Courier()
+    {
+        _count++;
+        Id = _count;
+    }
+}
---
+++ Magazine/Magazine/Services/ICargoService.cs
@@ -0,0 +1,8 @@
+﻿namespace Magazine.Services;
+
+public class ICargoService
+{
+    List<Courier> Couriers = new List<Courier>();
+    List<Customer> Customers = new List<Customer>();
+
+}
