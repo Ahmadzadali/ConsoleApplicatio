b7b361f5315e32b4aabe889734344a5aae89db0f
RiadAliyev38 <aliyevriad38@gmail.com>
2025-12-04 19:09:22 +0400

    add services

---
+++ Magazine/Magazine/Models/Courier.cs
@@ -0,0 +1,16 @@
+﻿namespace Magazine.Models;
+
+public class Courier
+{
+    private static int _count;
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool IsAvailable { get; set; }
+    public Courier()
+    {
+        _count++;
+        Id = _count;
+    }
+}
--- Magazine/Magazine/obj/Debug/net8.0/Magazine.AssemblyInfo.cs
+++ Magazine/Magazine/obj/Debug/net8.0/Magazine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magazine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4befff3d004094a3532622d5884c99ab4ca43c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354cd32da478c17a879f3aafbee46870051d85e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magazine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magazine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
